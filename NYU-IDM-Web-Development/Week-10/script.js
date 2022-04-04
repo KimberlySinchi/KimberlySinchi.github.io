bda6762f00925308b417428f357cfc23573adec1
KimberlySinchi <kimberlytoledo36@gmail.com>
2022-04-04 14:39:23 -0400

    ADD: Double click

--- NYU-IDM-Web-Development/Week-10/script.js
+++ NYU-IDM-Web-Development/Week-10/script.js
@@ -6,9 +6,9 @@ var theSenseImg = document.getElementsByClassName("sense");
 var theInteractiveWrapper = document.getElementById("interactive_wrapper");
 var theUserSpace = document.getElementById("user_space");
 
-var numEarsTextClicks = 0;
-var numEyesTextClicks = 0;
-var lruClick = 0; // 1 for ear text, 2 for eye text
+var numEarsConsecClicks = 0;
+var numEyesConsecClicks = 0;
+var lastClick = 0; // 1 for ear text, 2 for eye text
 
 // Ears text selected
 theEarsText.onclick = function changeContentEars() {
@@ -20,6 +20,7 @@ theEarsText.onclick = function changeContentEars() {
     // theInteractiveWrapper.style.filter = "drop-shadow(16px 16px 20px red) invert(75%);";
 
     theInteractiveWrapper.style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi2ejJ0KXLGz80aQezNGMNiyf47Mu5DmKavg&usqp=CAU)";
+    theWrapper.style.backgroundColor = "#843b62";
     // theEarsText.style.color = "pink";
     // theEarsText.style.fontSize = "10vh";
 
@@ -47,30 +48,47 @@ theEyesText.onclick = function changeContentEyes() {
 
 // Title (reset) selected
 theTitle.onclick = function changeContentEyes() {
+    titleContent();
+}
+
+// Tracking clicks
+theEarsText.addEventListener('click', function() {
+    // Double click functionality
+    if(lastClick == 1){
+        numEarsConsecClicks++;
+
+        if(numEarsConsecClicks%2 == 0)
+            titleContent();
+    }
+    else
+        numEarsConsecClicks = 1;
+
+    lastClick = 1;
+    console.log("Ear Clicks " + numEarsConsecClicks);
+});
+
+theEyesText.addEventListener('click', function() {
+    // Double click functionality
+    if(lastClick == 2){
+        numEyesConsecClicks++;
+
+        if(numEyesConsecClicks%2 == 0)
+            titleContent();
+    }
+    else
+        numEyesConsecClicks = 1;
+
+    lastClick = 2;
+    console.log("Eye Clicks " + numEyesConsecClicks);
+});
+
+// Title content gif
+function titleContent(){
     theSenseImg[0].src = "https://data.whicdn.com/images/140051440/original.gif";
     theSenseImg[1].src = "https://data.whicdn.com/images/140051440/original.gif";
 
     theTitle.textContent = "explore your senses";
     theInteractiveWrapper.style.backgroundImage = "url(https://media1.giphy.com/media/1YiJ9qOYgWPCQKhRjj/giphy.gif)";
-}
-
-// Tracking clicks
-theEarsText.addEventListener('click', function() {
-    numEarsTextClicks++;
-    lruClick = 1;
-    console.log("Ear Clicks " + numEarsTextClicks);
-});
-
-theEyesText.addEventListener('click', function() {
-    numEyesTextClicks++;
-    lruClick = 2;
-    console.log("Eye Clicks " + numEyesTextClicks);
-});
-
-// Reset gif
-function resetContent(){
-    theSenseImg[0].src = "https://data.whicdn.com/images/140051440/original.gif";
-    theSenseImg[1].src = "https://data.whicdn.com/images/140051440/original.gif";
 
     // theEarsText.style.color = "white";
     // theEarsText.style.fontSize = "4vh";
