6590d3b0145c05da564f7ff6a63947914a116dd9
KimberlySinchi <kimberlytoledo36@gmail.com>
2022-04-22 17:13:20 -0400

    ADD: Title init

--- NYU-Web-Dev/Final-Project/script.js
+++ NYU-Web-Dev/Final-Project/script.js
@@ -1,11 +1,24 @@
+// JS libraries utilized
+// https://www.typeitjs.com
+
 // CONSTANTS
 const KEYA = 65;
 const KEYZ = 90;
 const KEYBACKSPACE = 8;
+const KEYENTER = 13;
 
 // DOM elements
-var $word = $("#word");
-var $userStr = $("#userStr")
+var $container = $("#wrapper");
+var $word = $("<div />", {
+    id: "word"
+});
+var $userStr = $("<div />", {
+    id: "userStr"
+});
+var $title = $("<div />", {
+    id: "title",
+    text: "Space Racer"
+});
 
 // Script vars
 var URL = "https://random-word-api.herokuapp.com/word?length="
@@ -13,6 +26,25 @@ var word = "";
 var inputStr = "";
 var wordLen = 5;
 const activeWords = [];
+
+// GAME BUILDING FUNCTIONS
+const initTitle = () => {
+    console.log("BONK");
+    $container.append($title);
+    // titleTypeIt.go();
+}
+
+const initGame = () => {
+    $title.hide();
+    $container.append($word);
+    $container.append($userStr);
+    fetchWordOfLength(8);
+}
+
+// ANIMATION FUNCTIONS
+// const titleTypeIt = new TypeIt("#title", {
+//     strings: "Space Racer",
+// });
 
 // Using fetch API: https://medium.com/beginners-guide-to-mobile-web-development/the-fetch-api-2c962591f5c
 // https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
@@ -40,14 +72,14 @@ const fetchWordOfLength = (num) => {
         });
 }
 
-// Check if user spelled a word correctly
+// Check if user spelled a word correctly & updates accordingly
 const isWordSpelled = () => {
     for(let i = 0; i < activeWords.length; i++) {
         if(inputStr == activeWords[i]){
             activeWords.splice(i, 1);
             updateInput("");
             console.log(activeWords);
-            fetchWordOfLength(5);
+            fetchWordOfLength(10);
         }
     }
 }
@@ -60,13 +92,17 @@ const updateInput = (str) => {
 
 // Tracking current user input
 $(document).keydown(function(event) {
-    if(event.keyCode >= KEYA && event.keyCode <= KEYZ) {
-        updateInput(inputStr + event.key);
+    if($title.is(":visible") && event.keyCode == KEYENTER)
+        initGame();
+    else if($word.is(":visible")){
+        if(event.keyCode >= KEYA && event.keyCode <= KEYZ) {
+            updateInput(inputStr + event.key);
+        }
+        else if(event.keyCode == KEYBACKSPACE && inputStr != ""){
+            updateInput(inputStr.substring(0, inputStr.length-1));
+        }
+        isWordSpelled();
     }
-    else if(event.keyCode == KEYBACKSPACE && inputStr != ""){
-        updateInput(inputStr.substring(0, inputStr.length-1));
-    }
-    isWordSpelled();
 });
 
-fetchWordOfLength(8);
+initTitle();
