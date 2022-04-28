0046c9ba5452d3dcc0e1c6caede2fbcf569c215d
KimberlySinchi <kimberlytoledo36@gmail.com>
2022-04-28 10:07:36 -0400

    ADD: Game skeleton

--- NYU-Web-Dev/Final-Project/script.js
+++ NYU-Web-Dev/Final-Project/script.js
@@ -2,20 +2,22 @@
 // https://www.typeitjs.com
 
 // CONSTANTS
-const KEYA = 65;
-const KEYZ = 90;
-const KEYBACKSPACE = 8;
-const KEYENTER = 13;
+const WINDOW_H = $(window).height();
+const WINDOW_W = $(window).width();
+const KEY_A = 65;
+const KEY_Z = 90;
+const KEY_BACKSPACE = 8;
+const KEY_ENTER = 13;
 
 // DOM elements
-var $container = $("#wrapper");
-var $word = $("<div />", {
-    id: "word"
+var $container = $(".wrapper");
+var $asteroid = $("<div/>", {
+    id: "asteroid"
 });
-var $userStr = $("<div />", {
-    id: "userStr"
+var $userStr = $("<div/>", {
+    id: "user_str"
 });
-var $title = $("<div />", {
+var $title = $("<div/>", {
     id: "title",
     text: "Space Racer"
 });
@@ -26,6 +28,7 @@ var word = "";
 var inputStr = "";
 var wordLen = 5;
 const activeWords = [];
+const currAsteroids = [];
 
 // GAME BUILDING FUNCTIONS
 const initTitle = () => {
@@ -36,9 +39,9 @@ const initTitle = () => {
 
 const initGame = () => {
     $title.hide();
-    $container.append($word);
+    $container.attr("id", "game_wrapper");
     $container.append($userStr);
-    fetchWordOfLength(8);
+    createAsteroid(8);
 }
 
 // ANIMATION FUNCTIONS
@@ -46,12 +49,43 @@ const initGame = () => {
 //     strings: "Space Racer",
 // });
 
+// Creating and destroying asteroids (words)
+async function createAsteroid(num) {
+    // Awaiting a promise
+    const word = await fetchWordOfLength(num);
+    console.log(word);
+
+    // Creating an asteroid based off the return word
+    activeWords.push(word);
+    $newAsteroid = $asteroid.clone();
+    currAsteroids.push($newAsteroid);
+    console.log("CURRWORDS:", activeWords, "ASTEROIDS:", currAsteroids);
+    mruWord = activeWords[activeWords.length-1];
+    $container.append(currAsteroids[currAsteroids.length-1].text(mruWord));
+    $($newAsteroid).css({top: 0, left: (startCoords($newAsteroid))});
+    console.log("WINDOW_W", WINDOW_W, "STARTING:", startCoords($newAsteroid), "DIV SIZE:", $newAsteroid.width());
+}
+
+// DISTANCE FORMULA BING BONG TO CHECK WORDS OVERLAPPING
+// smaller words go faster bigger bois go slower
+
+const destroyAsteroid = (ind) => {
+    currAsteroids[ind].text("");
+    currAsteroids[ind].remove();
+    currAsteroids.splice(ind, 1);
+}
+
+// Asteroid velocity
+function startCoords($asteroid){
+    return Math.random() * (WINDOW_W - $asteroid.width());
+}
+
 // Using fetch API: https://medium.com/beginners-guide-to-mobile-web-development/the-fetch-api-2c962591f5c
 // https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
 // Fetching word
-const fetchWordOfLength = (num) => {
+async function fetchWordOfLength(num) {
     // Fetching the new word
-    fetch(URL + num.toString())
+    return await fetch(URL + num.toString())
         .then((response) => { // The response parameter takes the value of the object returned from fetch(URL)
             return response.json();
         })
@@ -59,30 +93,31 @@ const fetchWordOfLength = (num) => {
             console.log(data);
             return data[0];
         })
-        .then((currWord) => {
-            $word.text(currWord);
-
-            // TODO: NO REPEATS
-
-            activeWords.push(currWord); // Appending new word to array
-            console.log(activeWords); 
-        })
         .catch((err) => {
             console.log("Something went wrong!", err);
         });
 }
 
 // Check if user spelled a word correctly & updates accordingly
-const isWordSpelled = () => {
+const checkWordsSpelled = () => {
     for(let i = 0; i < activeWords.length; i++) {
         if(inputStr == activeWords[i]){
             activeWords.splice(i, 1);
             updateInput("");
-            console.log(activeWords);
-            fetchWordOfLength(10);
+            destroyAsteroid(i);
+            console.log(activeWords, currAsteroids);
+            createAsteroid(10);
         }
     }
 }
+
+// Determine if two asteroids would overlap
+const wouldOverlapInDeployment = ($asteroid) => {
+
+}
+
+// ONLY MAKE Y DIRECTION OF MOVEMENT
+// Have astronaunts walking on the bottom or something or conneccted to a ship
 
 // Update text displaying user input
 const updateInput = (str) => {
@@ -92,17 +127,22 @@ const updateInput = (str) => {
 
 // Tracking current user input
 $(document).keydown(function(event) {
-    if($title.is(":visible") && event.keyCode == KEYENTER)
+    if($title.is(":visible") && event.keyCode == KEY_ENTER)
         initGame();
-    else if($word.is(":visible")){
-        if(event.keyCode >= KEYA && event.keyCode <= KEYZ) {
+    else if($title.is(":hidden")){
+        if(event.keyCode >= KEY_A && event.keyCode <= KEY_Z)
             updateInput(inputStr + event.key);
-        }
-        else if(event.keyCode == KEYBACKSPACE && inputStr != ""){
+        else if(event.keyCode == KEY_BACKSPACE && inputStr != "")
             updateInput(inputStr.substring(0, inputStr.length-1));
-        }
-        isWordSpelled();
+        else if(event.keyCode == KEY_BACKSPACE)
+            createAsteroid(10);
+        checkWordsSpelled();
     }
+});
+
+$($container).click(function() {
+    if($title.is(":visible"))
+        initGame();
 });
 
 initTitle();
